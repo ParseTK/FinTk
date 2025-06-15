78693abceed77ff43ac4cca16784550d36f0a72d
TK <tjkirchner2401@student.stcc.edu>
2025-06-14 21:19:25 -0400

    Added API

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,18 +1,22 @@
-import React, { JSX, useState } from 'react';
+import React, { JSX, useState, MouseEvent } from 'react';
 
 interface Props {}
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
     const [getSearch, setSearch] = useState<string>("");
-    const onClick = (e: any) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
+    };
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      console.log(e);
     }
   return (
     <div>
-        <input value={getSearch} onChange={(e) => onClick(e)}> 
-
+        <input value={getSearch} onChange={(e) => handleChange(e)}> 
         </input>
+        <button onClick={(e) => onClick(e)} />
     </div>
   )
 }
---
+++ frontend/src/api.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { CompanySearch } from './company';
+
+interface SearchResponse {
+  data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Error fetching company data:', error.message);
+      return error.message;
+    } else {
+      console.error('Unexpected error:', error);
+      return 'An unexpected error occurred';
+    }
+  }
+};
